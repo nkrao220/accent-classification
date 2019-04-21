c206cb6a2379e1412d088b6392db16b105911a11
nkrao220 <nkrao220@gmail.com>
2019-04-21 13:25:56 -0600

    new eda for 3 class classification

--- src/eda_mozilla.py
+++ src/eda_mozilla.py
@@ -25,7 +25,8 @@ class Mfcc():
         self.col = col
 
     def mp3towav(self):
-        for filename in tqdm(self.df[self.col]):
+        uk = self.df[self.df['accent']=='england']
+        for filename in tqdm(uk[self.col]):
             pydub.AudioSegment.from_mp3("../data/clips/{}.mp3".format(filename)).export("../data/clips/wav/{}.wav".format(filename), format="wav")
 
     def wavtomfcc(self, file_path):
